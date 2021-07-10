5db72a5ebee9e65e54163157e86b78bc36edf1dd
jjuarez5 <julio.juarez5@outlook.com>
2021-07-09 19:37:34 -0500

    comment out smooth scrolling for now

--- js/main.js
+++ js/main.js
@@ -9,17 +9,17 @@ window.addEventListener("scroll", function () {
 });
 
 // Smooth Scrolling
-$("#navbar a, .btn").on("click", function (event) {
-  if (this.hash !== "") {
-    event.preventDefault();
+// $("#navbar a, .btn").on("click", function (event) {
+//   if (this.hash !== "") {
+//     event.preventDefault();
 
-    const hash = this.hash;
+//     const hash = this.hash;
 
-    $("html, body").animate(
-      {
-        scrollTop: $(hash).offset().top - 100,
-      },
-      800
-    );
-  }
-});
+//     $("html, body").animate(
+//       {
+//         scrollTop: $(hash).offset()
+//       },
+//       800
+//     );
+//   }
+// });
