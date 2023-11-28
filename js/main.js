106c3408c80aff94895a8de24664c2de8f0d29f3
jjuarez5 <julio.juarez5@outlook.com>
2023-11-28 00:28:02 -0600

    add chatbot chat box and scripts and styling

--- js/main.js
+++ js/main.js
@@ -8,6 +8,40 @@ window.addEventListener("scroll", function () {
   }
 });
 
+const chatbox = document.getElementById("chatbox");
+const userInput = document.getElementById("userInput");
+
+function appendMessage(sender, message, senderClass) {
+  const messageElement = document.createElement("div");
+  messageElement.classList.add("message");
+  messageElement.classList.add(senderClass);
+  messageElement.innerHTML = `<strong>${sender}:</strong> ${message}`;
+  chatbox.appendChild(messageElement);
+  console.info(" This is the body -> ", messageElement);
+}
+
+function sendMessage() {
+  const message = userInput.value;
+  appendMessage("You", message, "user-message");
+  userInput.value = "";
+  const userMessageObj = {
+    userMessage: message,
+  };
+
+  fetch("https://faqappservice.azurewebsites.net/api/FAQ", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(userMessageObj),
+  })
+    .then((response) => response.text())
+    .then((data) => {
+      const botMessage = data || "Sorry, I don't have an answer for that.";
+      appendMessage("FAQ Bot", botMessage, "bot-message");
+    });
+}
+
 // Smooth Scrolling
 // $("#navbar a, .btn").on("click", function (event) {
 //   if (this.hash !== "") {
